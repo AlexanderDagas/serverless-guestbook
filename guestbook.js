f52cb86f90decde1c3e49f25d68398cf5d658d3e
AlexanderDagas <77065014+AlexanderDagas@users.noreply.github.com>
2021-05-25 13:33:31 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a86c2de7.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
